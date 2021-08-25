1ebf94eda9e9eb9d01396fa1ae7fb35a0b2e9f43
tom1prog <castro.tomas.epet20@gmail.com>
2021-08-25 20:23:03 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/rendirfebrero_page.dart';
+import 'package:componentes/src/pages/rendirlibre_page.dart';
+import 'package:componentes/src/pages/certalumnoregular_page.dart';
+import 'package:componentes/src/pages/certalumnoretiro_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tramipet Componentes',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''),
+        Locale('es', 'ES'),
+      ],
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+        'rendir_febrero': (BuildContext context) => RendirFebreroPage(),
+        'rendir_libre': (BuildContext context) => RendirLibrePage(),
+        'cert_alumno_regular': (BuildContext context) =>
+            CertAlumnoRegularPage(),
+        'cert_alumno_retiro': (BuildContext context) => CertAlumnoRetiroPage(),
+      },
+    );
+  }
+}
